18dc79f239b8ae23822ee9fd83c92a0a79e8f708
Sheroll Faith <sherollfaith@gmail.com>
2025-06-15 22:50:10 +0530

    Day one completed

---
+++ Day-1/IdentifierDemo.java
@@ -0,0 +1,9 @@
+public class IdentifierDemo {
+    public static void main(String[] args)
+    {
+        int number = 20;
+        System.out.println("value of the number variable is : "+ number);
+        String studentName="Aniket";
+        System.out.println("value is : "+ studentName);
+    }
+}
