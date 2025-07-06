530050a1cf4e6a4b24d481ab5e3361a6e8e8c41f
Sheroll Faith <sherollfaith@gmail.com>
2025-07-07 00:23:47 +0530

    My Core Java Program

---
+++ src/Day11/v1/Demo.java
@@ -0,0 +1,32 @@
+package C2TC_CORE_JAVA_PROGRAM.src.Day11.v1;
+
+
+public class ArrayOperations {
+
+	int intArray[] ;
+	
+	public ArrayOperations() {
+		super();
+		
+	}
+
+	public ArrayOperations(int[] intArray) {
+		super();
+		this.intArray = intArray;
+		
+	}
+	
+	public void displayArray() {
+		for(int no:intArray) {
+			System.out.println("The array elements are: "+no);
+		}
+	}
+	
+	public int getElement(int position) {
+		return intArray[position];
+	}
+	
+	
+}
+
+
---
+++ src/Day11/v1/UserInterface.java
@@ -0,0 +1,20 @@
+package C2TC_CORE_JAVA_PROGRAM.src.Day11.v1;
+
+
+public class UserInterface {
+
+	public static void main(String[] args) {
+		try {
+			ArrayOperations obj= new ArrayOperations(new int[] {2,4,5,6});
+			obj.displayArray();
+			System.out.println("The element is: "+obj.getElement(2));
+			System.out.println("The element is: "+obj.getElement(10));
+		}catch(ArrayIndexOutOfBoundsException ae) {
+			System.err.println("Error!..."+ ae.getMessage());
+		}
+		
+	}
+
+}
+
+
