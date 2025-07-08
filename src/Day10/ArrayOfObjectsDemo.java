54ca92c8b25080bb20ef9aad2002791161988157
Sheroll Faith <sherollfaith@gmail.com>
2025-07-08 23:18:53 +0530

    my core java program

--- src/Day10/ArrayOfObjectsDemo.java
+++ src/Day10/ArrayOfObjectsDemo.java
@@ -10,13 +10,13 @@ public class ArrayOfObjectsDemo {
 		// allocating memory for 5 objects of type Student.
 		arr = new Student[5];
 		// initialize the first elements of the array
-		arr[0] = new Student();
+		arr[0] = new Student(101,"Sheroll");
 		// initialize the second elements of the array
-		arr[1] = new Student();
+		arr[1] = new Student(102,"Sheroll");
 		// so on...
-		arr[2] = new Student();
-		arr[3] = new Student();
-		arr[4] = new Student();
+		arr[2] = new Student(103,"Sheroll");
+		arr[3] = new Student(104,"Sheroll");
+		arr[4] = new Student(105,"Sheroll");
 		
 		// accessing the elements of the specified array
 		for (int i = 0; i < arr.length; i++)
